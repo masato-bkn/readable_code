7cf6aa3e782b3251ea1cb4262b55eae76e8a6a9e
重田 将人 <masato.0928.j@gmail.com>
2021-08-31 20:32:04 +0900

    特殊ケースの導入
    - UnKnownCustomerのpaymentHistoryでNullPaymentHistoryを返す
    - client3

--- 特殊ケースの導入/after.js
+++ 特殊ケースの導入/after.js
@@ -18,9 +18,15 @@ class UnKnownCustomer {
   get name() {return "occupant";}
   get billingPlan() {return registry.billingPlans.basic;}
   set billingPlan(arg) {/* 何もしない*/}
-  get paymentHistory() {}
 
-  get isUnknown() { return false; }
+  // 特殊ケース用オブジェクトは値オブジェクトなので、常に変更不可にすべき
+  get paymentHistory() {return new NullPaymentHistory();}
+
+  get isUnknown() { return true; }
+}
+
+class NullPaymentHistory {
+  get weeksDeliquantInLastYear() {return 0;}
 }
 
 function isUnknown(arg) {
@@ -30,7 +36,10 @@ function isUnknown(arg) {
 // client 1
 const aCustomer = site.customer;
 // ...
-let customerName = aCustomermer.name;
+let customerName = aCustomer.name;
 
 // client2
 const plan = aCustomer.billingPlan;
+
+// client3
+const weeksDeliquant = aCustomer.paymentHistory.weeksDeliquantInLastYear;
