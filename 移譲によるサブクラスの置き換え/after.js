2e461185905a50b0b316a699bd6eb16ad496c8b6
重田 将人 <masato.0928.j@gmail.com>
2021-09-11 08:40:01 +0900

    移譲によるサブクラスの置き換え
    - basePrice, hasDinnerをPremiumBookingDelegateに委譲

--- 移譲によるサブクラスの置き換え/after.js
+++ 移譲によるサブクラスの置き換え/after.js
@@ -9,9 +9,20 @@ class Booking {
         : this._show.hasOwnProperty('talkback') && !this.isPeakDay;
     }
     get basePrice() {
-      let result = this._show.price;
+      return (this._premiumDelegate)
+        ? this._premiumDelegate.basePrice
+        : this._privateBasePrice;
+    }
+
+    get _privateBasePrice() {
+     let result = this._show.price;
       if (this.isPeakDay) result += Math.round(result * 0.15);
       return result;
+    }
+    get hasDinner() {
+      return (this._premiumDelegate)
+      ? this._premiumDelegate.hasDinner
+      : undefined;
     }
     _bePremium(extras) {
       this._premiumDelegate = new PremiumBookingDelegate(this, extras)
@@ -38,7 +49,13 @@ class PremiumBookingDelegate {
   }
   get hasTalkBack() {
     return this._host._show.hasOwnProperty('talkback');
-  }  
+  } 
+  get basePrice() {
+    return Math.round(this._host._privateBasePrice + this._extra.premiumFee);
+  }
+  get hasDinner() {
+    return this._extras.hasOwnProperty('dinner') && !this._host.isPeakDay;
+  }
 }
 
 function createBooking(show, date) {
